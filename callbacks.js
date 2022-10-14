0a8255f557c03fc034cbf1138a61b58f164b47f8
YubrajKarki <yubrajkarki528@gmail.com>
2022-10-14 09:21:02 +0545

    callback function added

--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,32 @@
-const users = [{
+const users = [
+    {name: 'john', profession: 'developer'},
+    {name: 'peter', profession: 'doctor'},
+    {name: 'jane', profession: 'nurse'},
+]
+
+
+function getUsers() {
+
+    setTimeout(()=>{  users.forEach((user)=> {
+        console.log(user);
+    })}, 2000)
+
     
-}]
+}
+
+function createUsers(user, callback) {
+
+    setTimeout(()=> {
+        users.push(user);
+        callback();
+    }, 3000)
+    
+}
+
+const user = {name: 'Rajesh', profession: 'actor'};
+
+
+
+
+
+createUsers(user, getUsers);
